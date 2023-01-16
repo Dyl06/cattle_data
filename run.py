54f2a912ab75ba4f025d8000e1e9f4dec72e7a85
Dyl06 <dylhndrsn@gmail.com>
2023-01-16 23:40:01 +0000

    Remove unused functions and tidy up code. Fix some of the problems in problems tab. Run validation tests on application.

--- run.py
+++ run.py
@@ -50,7 +50,7 @@ class UserInputs:
         months weights. Data is added to an empty dictionary.
         """
         extra_cows = {}
-        new_feed = {}
+        new_feed = []
         while True:
             print("Please enter unique cow id")
             print("Please enter weights for the last three months.")
@@ -61,17 +61,17 @@ class UserInputs:
             cow_id = input("Enter cows unique id here:\n")
 
             cow_weights = input("Enter cattle weights here:\n")
-                
+
             weight_input = cow_weights.split(",")
-                
+
             if self.validate_weights_data(weight_input):
                 print("Weights are valid")
                 weight_input_int = [int(x) for x in weight_input]
                 extra_cows[cow_id] = weight_input_int
             else:
-                print("Invalid data. Please try again.\n")
+
                 continue
-        
+
             print("Would you like to enter another cow?")
             print("1) Yes")
             print("2) No")
@@ -102,20 +102,21 @@ class UserInputs:
         return (extra_cows, new_feed)
 
     def validate_weights_data(self, values):
-            """
-            Function to validate user inputs and return error if inputs provided
-            aren't correct.
-            """
+        """
+        Function to validate user inputs and return error if inputs provided
+        aren't correct.
+        """
 
-            try:
-                [int(value) for value in values]
-                if len(values) != 3:
-                    raise ValueError(
-                        f"Exactly 3 values required, you provided {len(values)}"
-                    )
-            except ValueError as e:
-                return False
-            return True
+        try:
+            [int(value) for value in values]
+            if len(values) != 3:
+                raise ValueError(
+                    f"Exactly 3 values required, you provided {len(values)}"
+                )
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again.\n")
+            return False
+        return True
 
 
 class CattleWeights:
@@ -174,24 +175,7 @@ class CattleFeed:
     def __init__(self, updated_feed):
         self.feed = updated_feed
 
-    def total_used_feed(self):
-        """
-        Function to calculate the total amount of feed used over the year.
-        """
-        consumption = sum(self.feed)
-        print(consumption)
-        return consumption
-
-    def feed_cost(self, total_consumed):
-        """
-        Function for the total cost of all the feed for the year. Assuming an
-        average cost of feed of £150 per ton.
-        """
-        cost = total_consumed * FEED_COST
-        print(cost)
-        return cost
-
-    def feed_conversion_ratio(self, DEC_INTAKE, dec_total, nov_total):
+    def feed_conversion_ratio(self, dec_intake, dec_total, nov_total):
         """
         Function to calculate how many kg's of food the average animal ate to
         put on 1kg of body weight. The function is divided by 1000 to convert
@@ -200,7 +184,7 @@ class CattleFeed:
         month to the next (Nov-Dec) and then dividing that by the amount of
         food eaten over the same period.
         """
-        fcr = round((DEC_INTAKE * 1000) / (dec_total - nov_total), 2)
+        fcr = round((dec_intake * 1000) / (dec_total - nov_total), 2)
         print(fcr)
         return fcr
 
@@ -255,30 +239,17 @@ class Main():
     Class to execute the entire aplication.
     """
     def __init__(self):
-
-        cows = {
-            "cow1": [10, 20, 30, 40, 150, 260],
-            "cow2": [11, 22, 33, 44, 155, 266],
-            "cow3": [15, 25, 35, 45, 155, 265]
-        }
-
-        feed = {
-            'jan': 1,
-            'feb': 1.1,
-            'nov': 1,
-            'dec': 1.2
-        }
-        dec_intake = feed['dec']
-
-        cows = {}
-        feed = {}
         
+        cows = {}
+        feed = []
+
         user_inputs = UserInputs()
         (extra_cows, new_feed) = user_inputs.user_cow_input()
         cows.update(extra_cows)
         feed = new_feed
-        #print(extra_cows)
-        #print(new_feed)
+        print(extra_cows)
+        print(new_feed)
+        dec_intake = feed[2]
 
         dec_weight = CattleWeights(cows, DEC_INDEX)
         dec_total = dec_weight.total_monthly_weight(cows, DEC_INDEX)
@@ -288,14 +259,12 @@ class Main():
         daily_gain = dec_weight.average_daily_gain(dec_total, nov_total)
 
         consumption = CattleFeed(feed)
-        total_consumed = consumption.total_used_feed()
-        feed_cost = consumption.feed_cost(total_consumed)
-        conversion_ratio = consumption.feed_conversion_ratio(dec_intake, dec_total,
-        nov_total)
+        conversion_ratio = consumption.feed_conversion_ratio(dec_intake,
+            dec_total, nov_total)
 
         report = Report(TARGET_WEIGHT, dec_total, daily_gain)
         time_left_report = report.remaining_time(TARGET_WEIGHT, avg_weight,
-        daily_gain)
+            daily_gain)
         target_feed = report.feed_to_target(avg_weight, conversion_ratio)
         cost_target = report.cost_to_target(target_feed)
 
